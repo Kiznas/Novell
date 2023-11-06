cab3e510dbcd9a23d54f14c4dc668de8ee2c5f94
Kinzas <64547234+Kiznas@users.noreply.github.com>
2023-11-06 18:01:09 +0100

    Initial commit

---
+++ Assets/Naninovel/Runtime/UI/ControlPanel/ControlPannelMapButton.cs
@@ -0,0 +1,16 @@
+﻿namespace Naninovel.UI
+{
+	public class ControlPanelMapButton : ScriptableLabeledButton
+	{
+		private IUIManager uiManager;
+
+		protected override void Awake ()
+		{
+			base.Awake();
+
+			uiManager = Engine.GetService<IUIManager>();
+		}
+
+		protected override void OnButtonClick () => uiManager.GetUI<CustomUI>()?.Show();
+	} 
+}
